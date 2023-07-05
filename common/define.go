138062dcc2c073ee094cfaf74e485447e07304e6
xiangbing <xiangbing@emain.cn>
2023-07-05 13:58:18 +0800

    upload

--- common/define.go
+++ common/define.go
@@ -2,4 +2,5 @@ package common
 
 const (
 	CommonURL = "https://gw-api.pinduoduo.com/api/router?type=%s&data_type=JSON&timestamp=%d&client_id=%s&sign=%s"
+	UploadURL = "https://gw-upload.pinduoduo.com/api/upload?type=%s&data_type=JSON&timestamp=%d&client_id=%s&sign=%s"
 )
