16e41eb4f81279fdb509477dcde6762519ceffb4
xiangbing <xiangbing@emain.cn>
2023-07-05 13:26:25 +0800

    update

---
+++ ddk/pdd.ddk.report.img.upload.go
@@ -0,0 +1,36 @@
+package ddk
+
+import (
+	"os"
+
+	"github.com/xiangwork/pinduoduo-sdk/common"
+	"github.com/xiangwork/pinduoduo-sdk/util"
+)
+
+type ReportImgUploadParams struct {
+	File os.File `json:"file"`
+}
+
+type ReportImgUploadPostResult struct {
+	Response struct {
+		Url string `json:"url"` //请求到的结果数
+	} `json:"response"`
+	common.CommonResult
+}
+
+func (d *DuoduoKe) ReportImgUploadPost(p *ReportImgUploadParams) (*ReportImgUploadPostResult, error) {
+	apiType := `pdd.ddk.report.img.upload`
+	params, paramsURL := util.FormatURLParams(p)
+	url := d.GetURL(apiType, "", params, paramsURL)
+	var result ReportImgUploadPostResult
+	err := util.HttpPOST(url, nil, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	err = common.CheckErrCode(result.CommonResult)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
---
+++ ddk/report_video_upload.go
@@ -0,0 +1,36 @@
+package ddk
+
+import (
+	"os"
+
+	"github.com/xiangwork/pinduoduo-sdk/common"
+	"github.com/xiangwork/pinduoduo-sdk/util"
+)
+
+type ReportVideoUploadParams struct {
+	File os.File `json:"file"`
+}
+
+type ReportVideoUploadPostResult struct {
+	Response struct {
+		Url string `json:"url"` //请求到的结果数
+	} `json:"response"`
+	common.CommonResult
+}
+
+func (d *DuoduoKe) ReportVideoUploadPost(p *ReportVideoUploadParams) (*ReportVideoUploadPostResult, error) {
+	apiType := `pdd.ddk.report.video.upload`
+	params, paramsURL := util.FormatURLParams(p)
+	url := d.GetURL(apiType, "", params, paramsURL)
+	var result ReportVideoUploadPostResult
+	err := util.HttpPOST(url, nil, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	err = common.CheckErrCode(result.CommonResult)
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
